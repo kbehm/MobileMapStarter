bbbc5b1834562be5df60f46d1ef3f729a6ae22c4
kbehm <kbehmvt@gmail.com>
2013-03-08 21:16:56 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,14 +5,14 @@
  * Basemap configurations
  */
 
-var DEFAULT_LAT =   44.5875;
-var DEFAULT_LNG = -123.1712;
+var DEFAULT_LAT = 44.02;
+var DEFAULT_LNG = -73.17;
 var DEFAULT_ZOOM = 15;
 
 var MIN_ZOOM = 10;
 var MAX_ZOOM = 18;
 
-var BING_API_KEY = "AjBuYw8goYn_CWiqk65Rbf_Cm-j1QFPH-gGfOxjBipxuEB2N3n9yACKu5s8Dl18N";
+var BING_API_KEY = "AnH69gg_XFoWEzjuqhZCcM3Zpa8Fo-iehnaWUMWZBGVa_AXJk3sjumaVAMMox7YO";
 
 var BASEMAPS = {};
 BASEMAPS['terrain']    = new L.TileLayer("http://{s}.tiles.mapbox.com/v3/greeninfo.map-fdff5ykx/{z}/{x}/{y}.jpg", { subdomains:['a','b','c','d'] });
